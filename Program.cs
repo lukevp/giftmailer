466dbda39eceb1c3b6b4887c804345c78502fb7b
Luke Paireepinart <luke@paireepinart.com>
2016-12-09 11:36:12 -0600

    - Added default command line project - updated .gitignore to ignore /bin/ and /obj/

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftMailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
